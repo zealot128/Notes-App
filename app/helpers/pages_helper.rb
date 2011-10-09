7d8d572b96af901193bac5abdee488f88877ba1f
Stefan Wienert <stwienert@gmail.com>
2011-10-09 10:47:44 +0200

    deleted unessecary files for a base app

--- app/helpers/pages_helper.rb
+++
@@ -1,5 +0,0 @@
-module PagesHelper
-  def gemfile_credits
-    File.read("#{Rails.root}/Gemfile").split("\n").select{|i| i.match /^\s*gem/}.map{|i| i.split(" ")[1].gsub("'","").gsub('"',"").gsub(",","")}.sort_by{|i| i.downcase}.join(", ")
-  end
-end
--- db/migrate/20110909074144_add_link_to_notes.rb
+++
@@ -1,5 +0,0 @@
-class AddLinkToNotes < ActiveRecord::Migration
-  def change
-    add_column :notes, :link, :string
-  end
-end
